fec13fbce5f15395c36c3dd828be2f6bf8b20684
pierangela <pierangela@pierangela-VGN-FW21E>
2014-04-03 11:18:09 +0200

    nemici

---
+++ Copy of CASSIOPEGGIAMI/src/Entity/Nemico.java
@@ -0,0 +1,50 @@
+package Entity;
+
+import TileMap.mattonciniMap;
+
+
+
+public class Nemico extends MapObject {
+	
+	protected int health; //in teoria non dovrebbe morire subito al primo attacco. o forse si?????!!!
+	protected int maxHealth;
+	protected boolean dead;
+	protected int damage; //pericolosità
+	
+	protected boolean indietreggiare;
+	protected long indietreggiareTimer;
+	
+	public Nemico(mattonciniMap tm) {
+		super(tm);
+	}
+	
+	public boolean isDead() { return dead; }
+	
+	public int getDamage() { return damage; } //il dannoche arreca il nemico
+	
+	public void hit(int damage) {
+		if(dead || indietreggiare) return;
+		health -= damage;
+		if(health < 0) health = 0;
+		if(health == 0) dead = true;
+		indietreggiare = true;
+		indietreggiareTimer = System.nanoTime();
+	}
+	
+	public void update() {}
+	
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+

01b70a7720388dd3de38a4ca56daeb78ac6a49c8
pierangela <pierangela@pierangela-VGN-FW21E>
2014-04-06 12:33:06 +0200

    + barra vite, + nemici, + fitoplancccumicazzusichiama

---
+++ Copy of CASSIOPEGGIAMI/src/Entity/BarraVita.java
@@ -0,0 +1,59 @@
+package Entity;
+
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import javax.imageio.ImageIO;
+
+public class BarraVita {
+	
+	private Cassiopea cassiopea;
+	
+	private BufferedImage image;
+	private Font font;
+	
+	public BarraVita(Cassiopea p) {
+		cassiopea = p;
+		try {
+			image = ImageIO.read(
+				getClass().getResourceAsStream(
+					"/BarraVita/barraVita.gif"
+				)
+			);
+			font = new Font("Arial", Font.PLAIN, 14);
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void draw(Graphics2D g) {
+		
+		g.drawImage(image, 0, 10, null);
+		g.setFont(font);
+		g.setColor(Color.WHITE);
+		g.drawString(
+			cassiopea.getHealth() + "/" + cassiopea.getMaxHealth(),
+			30,
+			25
+		);
+//		g.drawString(
+//			cassiopea.getFire() / 100 + "/" + cassiopea.getMaxFire() / 100,
+//			30,
+//			45
+//		);
+		
+	}
+	
+}
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Copy of CASSIOPEGGIAMI/src/Entity/Fitoplancton.java
@@ -0,0 +1,144 @@
+package Entity;
+
+
+
+import TileMap.mattonciniMap;
+
+import java.awt.image.BufferedImage;
+import java.awt.Graphics2D;
+
+import javax.imageio.ImageIO;
+
+public class Fitoplancton extends Nemico {
+	
+	private BufferedImage[] sprites;
+	
+	public Fitoplancton(mattonciniMap tm) {
+		
+		super(tm);
+
+		
+		moveSpeed = 0.3;
+		maxSpeed = 0.3;
+		fallSpeed = 0.2;
+		maxFallSpeed = 10.0;
+		
+		width = 30;
+		height = 30;
+		cwidth = 20;
+		cheight = 20;
+		
+		health = maxHealth = 2;
+		damage = 1;
+		
+		try {
+			
+			BufferedImage spritesheet = ImageIO.read(
+				getClass().getResourceAsStream(
+					"/Punti/fitopl.gif"
+				)
+			);
+			
+			sprites = new BufferedImage[3];
+			for(int i = 0; i < sprites.length; i++) {
+				sprites[i] = spritesheet.getSubimage(
+					i * width,
+					0,
+					width,
+					height
+				);
+			}
+			
+			
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+		}
+		
+		animation = new Animation();
+		animation.setFrames(sprites);
+		animation.setDelay(300);
+		
+		right = true;
+		facingRight = true;
+		
+	}
+	
+	private void getNextPosition() {
+		
+//		// movement
+//		if(left) {
+//			dx -= moveSpeed;
+//			if(dx < -maxSpeed) {
+//				dx = -maxSpeed;
+//			}
+//		}
+//		else if(right) {
+//			dx += moveSpeed;
+//			if(dx > maxSpeed) {
+//				dx = maxSpeed;
+//			}
+//		}
+//		
+//		// VOLAREEEEEEEEEE
+//		if(falling) {
+//			dy += fallSpeed;
+//		}
+		
+	}
+	
+	public void update() {
+		
+		// update position
+		getNextPosition();
+		checkmattonciniMapCollision();
+		setPosition(xtemp, ytemp);
+		
+		//  indietreggiaare
+//		if(indietreggiare) {
+//			double elapsed =
+//				(System.nanoTime() - indietreggiareTimer) / 1000000;
+//			if(elapsed > 400) {
+//				indietreggiare = false;
+//			}
+//		}
+//		
+//		
+//		if(right && dx == 0) {
+//			right = false;
+//			left = true;
+//			facingRight = false;
+//		}
+//		else if(left && dx == 0) {
+//			right = true;
+//			left = false;
+//			facingRight = true;
+//		}
+//		
+//		// update animation
+		animation.update();
+		
+	}
+	
+	public void draw(Graphics2D g) {
+		
+		//if(notOnScreen()) return;
+		
+		setMapPosition();
+		
+		super.draw(g);
+		
+	}
+	
+}
+
+
+
+
+
+
+
+
+
+
+
